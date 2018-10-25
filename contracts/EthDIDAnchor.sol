020101561fa72bb208f1ea32f200106bdc189ea8
Justin Guyton <guyton.justin@gmail.com>
2018-10-25 17:40:09 -0400

    updated contract to log txn number

--- contracts/EthDIDAnchor.sol
+++ contracts/EthDIDAnchor.sol
@@ -2,16 +2,23 @@ pragma solidity ^0.4.21;
 
 contract EthDIDAnchor {
 
-    mapping (bytes32 => bool) anchors;
+    // Mapping anchor hash to the ipfs hash
+    mapping (bytes32 => bytes32) anchors;
 
-    event AnchorHashCreated(bytes32 anchorHash);
+    // Mapping the txnNumber to anchorHash
+    bytes32[] public transactions;
+    uint public transactionNumber = 0;
 
-    function newAnchorHash(bytes32 _anchorHash) public {
-        anchors[_anchorHash] = true;
-        emit AnchorHashCreated(_anchorHash);
+    event AnchorHashCreated(bytes32 anchorHash, bytes32 ipfsHash);
+
+    function newAnchorHash(bytes32 _anchorHash, bytes32 _ipfsHash) public {
+        anchors[_anchorHash] = _ipfsHash;
+        transactionNumber++;
+        transactions[transactionNumber] = _anchorHash;
+        emit AnchorHashCreated(_anchorHash, _ipfsHash);
     }
 
-    function doesHashExist(bytes32 _anchorHash) public view returns(bool) {
+    function doesHashExist(bytes32 _anchorHash) public view returns(bytes32) {
         return anchors[_anchorHash];
     }
 
